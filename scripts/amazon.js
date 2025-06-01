eb9816a660e140662ba270d91857618f4b4980ba
Srinu-bhimavarapu <srinubhimavarapu090@gmail.com>
2025-06-01 23:06:46 +0530

    small changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
-const products = [
+/*const products = [
 {
-image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+image : 'images/products/athletic-cotton-socks-6-pair',
 name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
 ratings: {
 stars:4.5,
@@ -10,8 +10,8 @@ count : 87
 },
 
 {
-image : 'images/products/intermediate-composite-basketball.jpg',
-name : 'Intermediate Size Basketball',
+  image : 'images/products/intermediate-composite-basketball.jpg',
+ name : 'Intermediate Size Basks.jpg', 
 ratings : {
 stars : 4,
 count : 127
@@ -32,20 +32,41 @@ priceCents : 799
 {
 image: 'images/products/black-2-slot-toaster.jpg',
 name: '2 Slot Toaster - Black',
-ratings:{
-stars:5,
-count:2197
+ratings :{
+stars : 5,
+count : 2197
 
 },
 
 priceCents: 1899
     
-}
-];
-let productsHTML = ''
+},
+{
+  image : 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name : '6 Piece White Dinner Plate Set',
+
+ratings:{
+stars : 5,
+count:2197
+    },
+    priceCents:1899
+
+
+},
+
+];*/
+/*import { cart,addtoCart } from "../data/cart";
+import { products } from "../data/products";
+import { formatCurrency } from "./utils/money";
+import {}
+*/
+import {cart, addtoCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
+let productsHTML = '';
 products.forEach((product) => {
 
-productsHTML+= `
+productsHTML += `
 
         <div class="product-container">
           <div class="product-image-container">
@@ -62,12 +83,12 @@ productsHTML+= `
               src="images/ratings/rating-${
                product.rating.stars * 10 }.png">
             <div class="product-rating-count link-primary">
-            ${product.ratings.count}
+            ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCents/100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -92,7 +113,8 @@ productsHTML+= `
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -102,4 +124,86 @@ productsHTML+= `
 });
 console.log(productsHTML);
 
-document.querySelector('.js-product-grid').innerHTML=productsHTML
+document.querySelector('.js-products-grid').
+innerHTML = productsHTML;
+//here to stop the code for home page of amazon
+
+// here to start code for add to cart
+
+/*function addtoCart(productId) {
+  let matchingItem;
+    cart.forEach((cartitem)=>{
+      if(productId === cartitem.productId){
+        matchingItem=cartitem;
+      }
+    });                                  //this code moves to cart.js
+    if(matchingItem){
+      matchingItem.quantity +=1;
+    }
+      else {
+        cart.push({
+          productId : productId,
+          quantity: 1
+        });
+
+      }
+}*/
+
+
+
+
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button)=>{
+  button.addEventListener('click',()=>{
+    const productId=button.dataset.productId;
+   addtoCart(productId);
+
+   /* let matchingItem;
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        matchingItem=item;
+      }
+    });
+    if(matchingItem){
+      matchingItem.quantity +=1;
+    }
+      else {
+        cart.push({
+          productId : productId,
+          quantity: 1
+        });
+
+      }*/
+
+
+
+  }); 
+});
+
+function updateCartQuantity(){
+
+let cartQuantity=0;
+
+cart.forEach((cartitem)=>{
+cartQuantity+=cartitem.quantity;
+});
+
+document.querySelector('.js-cart-quantity')
+.innerHTML=cartQuantity;
+
+
+
+
+}
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) =>{
+button.addEventListener('click',() =>{
+
+const productid = button.dataset.
+productid;
+addtoCart(productid);
+updateCartQuantity();
+});
+
+});
